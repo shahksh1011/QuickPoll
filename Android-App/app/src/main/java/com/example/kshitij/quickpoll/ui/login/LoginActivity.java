35d416e20d09f68aa5995655f9e45ea9acb45ea7
Dikshali <dikshali.margaj@gmail.com>
2019-12-08 16:51:02 -0500

    call main activity on signin

--- Android-App/app/src/main/java/com/example/kshitij/quickpoll/ui/login/LoginActivity.java
+++ Android-App/app/src/main/java/com/example/kshitij/quickpoll/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.kshitij.quickpoll.MainActivity;
 import com.example.kshitij.quickpoll.R;
 import com.example.kshitij.quickpoll.ui.login.LoginViewModel;
 import com.example.kshitij.quickpoll.ui.login.LoginViewModelFactory;
@@ -120,6 +122,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
